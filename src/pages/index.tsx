d3de78fe96e9667ade5550785aade1db3a0103e5
Goemon5 <119960019+Goemon5@users.noreply.github.com>
2024-10-14 17:24:10 +0900

    homeメッセージを設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
 import Layout from "@/components/layout/Layout ";
+import styles from "../styles/pages/index.module.css";
 
 export default function Home() {
   return (
     <div>
       <Layout>
-        <h1>Welcome to Some Page</h1>
+        <div>
+          <img
+            src="/images/実験.jpg"
+            className={styles.productDetailTopLeftImage} // CSS Modulesのクラスを適用
+          />
 
-        <p>This is the content of the page.</p>
+          <div className="p-[20px]">
+            <h1 className="text-left font-bold text-[30px] ml-3px">
+              エンジニアの「学び方」がわかる
+            </h1>
+            <p className="text-left text-[20px] mt-8px color: #6a6f73;">
+              あなたの学び方に合わせた学習方法を見つけよう
+            </p>
+          </div>
+        </div>
       </Layout>
     </div>
   );
