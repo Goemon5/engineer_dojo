8386f01a3ad1de20512a9a200ca9868e9eee12b8
Goemon5 <119960019+Goemon5@users.noreply.github.com>
2024-10-14 15:20:54 +0900

    不要なコードを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,11 @@
-import Image from "next/image";
-import localFont from "next/font/local";
-import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
 import Layout from "@/components/layout/Layout ";
-import FamilyInput from "@/components/FamilyInput";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function Home() {
   return (
     <div>
       <Layout>
         <h1>Welcome to Some Page</h1>
-        <FamilyInput />
+
         <p>This is the content of the page.</p>
       </Layout>
     </div>
