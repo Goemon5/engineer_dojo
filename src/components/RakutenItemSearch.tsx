a0d5f69e167777e846fffd90d518d5de58722848
Goemon5 <119960019+Goemon5@users.noreply.github.com>
2024-11-01 11:27:22 +0900

    サブカテゴリーからのみ検索

--- src/components/RakutenItemSearch.tsx
+++ src/components/RakutenItemSearch.tsx
@@ -30,7 +30,7 @@ const RakutenItemSearch: React.FC<RakutenItemSearchProps> = ({
         "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601";
       const APPLICATION_ID = "";
       const AFFILIATE_ID = "412ba417.480adb3f.412ba418.b4dfc510";
-      const searchKeyword = `${category} ${subcategory}`;
+      const searchKeyword = `${subcategory}`;
 
       const response = await fetch(
         `${API_URL}?applicationId=${APPLICATION_ID}&affiliateId=${AFFILIATE_ID}&keyword=${searchKeyword}&hits=10`
