6ddc7b5bb1d9d6c92a5c2dabc6a364e45d90948f
Goemon5 <119960019+Goemon5@users.noreply.github.com>
2024-11-02 15:19:54 +0900

    必要ないaタグを削除

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -10,26 +10,12 @@ function Footer({}: Props) {
           href="https://www.rakuten.co.jp/"
           className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
         >
-          © 2023 Example Inc. All Rights Reserved.
+          © 2024 Example Inc. All Rights Reserved.
         </a>
         <ul className="flex flex-wrap items-center mb-6 text-sm font-normal sm:mb-0 dark:text-gray-400">
-          <li>
-            <a
-              href="https://corp.rakuten.co.jp/about/"
-              className="text-gray-600 hover:underline hover:text-crimson-red me-4 md:me-6"
-            >
-              サービスについて
-            </a>
-          </li>
+          <li>サービスについて</li>
           <li></li>
-          <li>
-            <a
-              href="https://ichiba.faq.rakuten.net/"
-              className="text-gray-600 hover:underline hover:text-crimson-red"
-            >
-              お問い合わせ
-            </a>
-          </li>
+          <li>お問い合わせ</li>
         </ul>
       </div>
     </div>
