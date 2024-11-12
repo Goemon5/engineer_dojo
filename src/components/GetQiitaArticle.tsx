0113ebb501ce5e210e84754ed1771ffbe3555e28
Goemon5 <119960019+Goemon5@users.noreply.github.com>
2024-11-12 18:30:11 +0900

    qiitaの欄にpagenationを追加

--- src/components/GetQiitaArticle.tsx
+++ src/components/GetQiitaArticle.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ArticleCard from "./ArticleCard";
+import styles from "@/styles/RakutenItemSearch.module.css";
 
 interface QiitaArticlProps {
   subcategory: string;
@@ -8,6 +9,8 @@ interface QiitaArticlProps {
 const QiitaArticles: React.FC<QiitaArticlProps> = ({ subcategory }) => {
   const [articles, setArticles] = useState<any[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const [index, setIndex] = useState(0);
+  const itemsPerPage = 5;
 
   useEffect(() => {
     const fetchArticles = async () => {
@@ -36,6 +39,18 @@ const QiitaArticles: React.FC<QiitaArticlProps> = ({ subcategory }) => {
 
     fetchArticles();
   }, [subcategory]);
+  const handleNext = () => {
+    if (index + itemsPerPage < articles.length) {
+      setIndex(index + itemsPerPage);
+    }
+  };
+
+  // 前のセットに戻る関数
+  const handlePrev = () => {
+    if (index - itemsPerPage >= 0) {
+      setIndex(index - itemsPerPage);
+    }
+  };
 
   if (error) {
     return <div>Error: {error}</div>;
@@ -45,14 +60,42 @@ const QiitaArticles: React.FC<QiitaArticlProps> = ({ subcategory }) => {
     <div>
       <h1>Qiita Articles</h1>
       <div style={{ display: "flex", flexWrap: "wrap", gap: "1rem" }}>
-        {articles.map((article) => (
-          <ArticleCard
-            key={article.id}
-            title={article.title}
-            description={article.body.substring(0, 50)} // 100文字まで表示
-            url={article.url}
-          />
-        ))}
+        <div className={styles.cardList}>
+          <div className={styles.navButtons}>
+            {index > 0 && (
+              <button onClick={handlePrev} className={styles.navButton}>
+                &#10094;
+              </button>
+            )}
+          </div>
+
+          <ul className={styles.itemList}>
+            {articles && articles.length > 0 ? (
+              articles
+                .slice(index, index + itemsPerPage)
+                .map((article, idx) => (
+                  <li key={idx} className={styles.listItem}>
+                    <ArticleCard
+                      key={article.id}
+                      title={article.title}
+                      description={article.body.substring(0, 50)} // 100文字まで表示
+                      url={article.url}
+                    />
+                  </li>
+                ))
+            ) : (
+              <p>No items found.</p>
+            )}
+          </ul>
+
+          <div className={styles.navButtons}>
+            {index + itemsPerPage < articles.length && (
+              <button onClick={handleNext} className={styles.navButton}>
+                &#10095;
+              </button>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
