1bc9d5945b36e816c32d448e52a747921da04b2c
Goemon5 <119960019+Goemon5@users.noreply.github.com>
2024-11-18 23:54:25 +0900

    雛形を作成

---
+++ src/pages/Simulation.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Checklist from "@/components/CheckList";
+
+interface Props {}
+
+export const Simulation = (props: Props) => {
+  return (
+    <div>
+      <Checklist />
+    </div>
+  );
+};
