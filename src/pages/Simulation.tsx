77a35316ac76cba5eb731d7dd7e6747950709ff0
Goemon5 <119960019+Goemon5@users.noreply.github.com>
2024-11-19 23:46:34 +0900

    今後使う関数をわかるだけ配置

--- src/pages/Simulation.tsx
+++ src/pages/Simulation.tsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { useState } from "react";
 import Checklist from "@/components/CheckList";
 
 interface Props {}
 
-const Simulation: React.FC<Props> = (props) => {
+const Simulation: React.FC<Props> = () => {
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [familyDetails, setFamilyDetails] = useState([
+    { gender: -1, age_group: -1 },
+  ]);
+  const handleGenderChange = (index: number, value: string) => {
+    const newDetails = [...familyDetails];
+    newDetails[index].gender = parseInt(value, 10);
+    setFamilyDetails(newDetails);
+  };
+
+  const handleAgeGroupChange = (index: number, value: string) => {
+    const newDetails = [...familyDetails];
+    newDetails[index].age_group = parseInt(value, 10);
+    setFamilyDetails(newDetails);
+  };
+
   const mockChecklistItems = [
     { id: 1, name: "Water", category_id: 1 },
     { id: 2, name: "Food", category_id: 1 },
