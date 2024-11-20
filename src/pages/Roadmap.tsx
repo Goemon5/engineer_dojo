db626edce9940c78a540b2e53e6ba8826ba27091
Goemon5 <119960019+Goemon5@users.noreply.github.com>
2024-11-20 16:27:43 +0900

    roadmapページの雛形を作成

---
+++ src/pages/Roadmap.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useState } from "react";
+import Checklist from "@/components/CheckList";
+import Layout from "@/components/layout/Layout ";
+import Simulation from "@/components/Simulation";
+import Image from "next/image";
+import Road from "/Users/takeuchidaiki/engineer_dojo/public/images/roadmap.jpg";
+
+interface Props {}
+
+const RoadMap: React.FC = () => {
+  return (
+    <div>
+      <Layout>
+        <div>
+          <Image src={Road} alt="RoadMap" />
+
+          <div className="p-[20px]">
+            <h1 className="text-left font-bold text-[30px] ml-3px">
+              あなたに最適なロードマップを作ろう
+            </h1>
+            <p className="text-left text-[20px] mt-8px color: #6a6f73;">
+              あなたの学び方に合わせた学習方法を見つけよう
+            </p>
+          </div>
+          <Simulation />
+        </div>
+      </Layout>
+    </div>
+  );
+};
+export default RoadMap;
